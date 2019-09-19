b38264ec26d8af70ae99098f5a283f135e980b32
Administrator <Administrator@tarena>
2019-09-19 11:52:53 +0800

    添加注释//123

--- src/main/java/com/cy/Application.java
+++ src/main/java/com/cy/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	//123
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
